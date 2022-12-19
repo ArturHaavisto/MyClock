4fb9a098a2a2779c6cd47a31fbabdf1a0efda2b5
ArturHaavisto <arturhaavisto1@gmail.com>
2022-12-19 20:50:43 +0200

    add text and two buttons

--- MyClock/app/src/main/java/com/example/myclock/MainActivity.kt
+++ MyClock/app/src/main/java/com/example/myclock/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myclock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myclock.ui.theme.MyClockTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,7 @@ class MainActivity : ComponentActivity() {
             MyClockTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CardWithShape()
                 }
             }
         }
@@ -27,14 +30,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CardWithShape() {
+    //val paddingModifier = Modifier.padding(10.dp)
+    Card(
+        shape = RoundedCornerShape(20.dp),
+        elevation = 10.dp,
+        //modifier = paddingModifier
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Row {
+            Text(text = "Round corner shape", Modifier.padding(10.dp))
+            var onState by remember { mutableStateOf(true) }
+
+            Button(
+                onClick = { onState = !onState }
+            ) {
+                Text(if (onState) "Start" else "Pause")
+            }
+            Button(
+                onClick = { /* handle click event */ }
+            ) {
+                Text("End")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyClockTheme {
-        Greeting("Android")
+        CardWithShape()
     }
 }
